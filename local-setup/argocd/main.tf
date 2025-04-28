77932202fd3ca6fb882d6d65b2cdda5c11b8f967
Abdelhak Marouane <am0089@uah.edu>
2025-04-28 10:03:24 -0500

    Minimizing support provate repos

--- local-setup/argocd/main.tf
+++ local-setup/argocd/main.tf
@@ -1,11 +1,13 @@
 resource "local_file" "argocd_values" {
   filename = "${path.root}/argocd/argocd-conf/values.yaml"
   content = templatefile("${path.root}/argocd/argocd-conf/values.yaml.tmpl", {
-    github_app_id             = var.github_app_id
+    argocd_applications = var.argocd_applications
+    github_app_id = var.github_app_id
     github_app_installation_id = var.github_app_installation_id
-    github_app_private_key    = var.github_app_private_key
+    github_app_private_key = var.github_app_private_key
   })
 }
+
 
 resource "helm_release" "argocd" {
   name             = "argocd"
@@ -31,50 +33,5 @@ resource "null_resource" "argocd-ingess" {
   provisioner "local-exec" {
     working_dir = "./argocd"
     command     = "sleep 30 && kubectl apply -f ./argocd-conf/argocd-ingress.yaml"
-  }
-}
-
-locals {
-  # Combine the default application with the list of additional applications
-  combined_applications = concat(
-    length(var.argocd_applications) > 0 ? var.argocd_applications : [{
-      app_name      = var.app_name
-      project_name  = var.project_name
-      repo_url      = var.repo_url
-      target_path   = var.target_path
-      target_branch = var.target_branch
-      namespace     = "default"
-      github_app_id = var.github_app_id
-      github_app_installation_id = var.github_app_installation_id
-    }]
-  )
-
-  all_applications = join("\n---\n", [
-    for app in local.combined_applications : templatefile("${path.root}/argocd/argocd-conf/argocd-github-app.yaml.tmpl", {
-      app_name      = app.app_name
-      project_name  = app.project_name
-      repo_url      = app.repo_url
-      target_branch = app.target_branch
-      target_path   = app.target_path
-      github_app_id = var.github_app_id
-      github_app_installation_id = var.github_app_installation_id
-    })
-  ])
-}
-
-# Create a single file containing all applications
-resource "local_file" "all_argocd_applications" {
-  filename = "${path.root}/argocd/argocd-conf/all-applications.yaml"
-  content  = local.all_applications
-}
-
-resource "null_resource" "argocd-github-conf" {
-  depends_on = [null_resource.argocd-ingess, local_file.all_argocd_applications]
-  triggers = {
-    config_hash = sha256(local.all_applications)
-  }
-  provisioner "local-exec" {
-    working_dir = "./argocd"
-    command     = "kubectl apply -f ./argocd-conf/all-applications.yaml"
   }
 }
--- local-setup/argocd/variables.tf
+++ local-setup/argocd/variables.tf
@@ -5,35 +5,13 @@ variable "argocd_applications" {
     repo_url      = string
     target_path   = string
     target_branch = string
+    private       = optional(bool, false)
     namespace     = optional(string, "default")
   }))
   description = "List of ArgoCD applications to create"
   default = []
 }
 
-variable "app_name" {
-  type        = string
-  description = "github Application Name"
-}
-
-variable "project_name" {
-  type        = string
-  description = "ArgoCD Project Name"
-}
-
-variable "target_branch" {
-  type        = string
-  description = "Target Branch Name"
-}
-
-variable "repo_url" {
-  type        = string
-  description = "Repo URL"
-}
-variable "target_path" {
-  type        = string
-  description = "Repo target path"
-}
 
 variable "github_app_id" {
   description = "GitHub App ID"
