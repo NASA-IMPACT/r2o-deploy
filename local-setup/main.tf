8d0a76f7c82dd445812fbfadc8a6814d53ec5abe
Abdelhak Marouane <am0089@uah.edu>
2025-05-18 13:40:18 -0500

    Switch provision

--- local-setup/main.tf
+++ local-setup/main.tf
@@ -8,7 +8,7 @@ module "manual-setup" {
 
 module "kind" {
   source                     = "./kind"
-  count                      = var.manual_setup ? 1 : 0
+  count                      = var.manual_setup ? 0 : 1
   cluster_name               = var.cluster_name
   http_ingress_port          = var.http_ingress_port
   https_ingress_port         = var.https_ingress_port
@@ -19,7 +19,7 @@ module "kind" {
 
 module "argocd" {
   source                      = "./argocd"
-  count                       = var.manual_setup ? 1 : 0
+  count                       = var.manual_setup ? 0 : 1
   depends_on                  = [module.kind]
   argocd_applications         = var.argocd_applications
   github_app_private_key_path = var.path_to_github_app_private_key
