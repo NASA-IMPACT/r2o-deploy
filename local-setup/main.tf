12ae5e803d818242f6411ee3d039b017f360ed34
Abdelhak Marouane <am0089@uah.edu>
2025-05-18 13:31:16 -0500

    include manual setup

--- local-setup/main.tf
+++ local-setup/main.tf
@@ -8,6 +8,7 @@ module "manual-setup" {
 
 module "kind" {
   source                     = "./kind"
+  count                      = var.manual_setup ? 1 : 0
   cluster_name               = var.cluster_name
   http_ingress_port          = var.http_ingress_port
   https_ingress_port         = var.https_ingress_port
@@ -18,6 +19,7 @@ module "kind" {
 
 module "argocd" {
   source                      = "./argocd"
+  count                       = var.manual_setup ? 1 : 0
   depends_on                  = [module.kind]
   argocd_applications         = var.argocd_applications
   github_app_private_key_path = var.path_to_github_app_private_key
