6b5551a8bab5951c3db0be54c02e17cc89818c48
Abdelhak Marouane <am0089@uah.edu>
2025-05-19 09:28:54 -0500

    Add new repo

--- local-setup/variables.tf
+++ local-setup/variables.tf
@@ -14,26 +14,6 @@ variable "https_ingress_port" {
   default = 4444
 }
 
-variable "argocd_applications" {
-  type = list(object({
-    app_name      = string
-    project_name  = optional(string)
-    repo_url      = string
-    target_path   = string
-    target_branch = string
-    private       = optional(bool)
-  }))
-  description = "List of ArgoCD applications to create"
-  default     = [
-    {
-      app_name      = "nginx-app"
-      repo_url      = "https://github.com/NASA-IMPACT/r2o-fastapi-k8s"
-      target_path   = "fastapi-manifest"
-      target_branch = "use-ssl"
-      private       = true
-    }
-  ]
-}
 
 variable "github_app_id" {
   description = "GitHub App ID"
