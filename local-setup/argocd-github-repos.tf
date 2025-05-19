e0f6fef2f36ee914426e9b5591e5225d6bd01d3a
Abdelhak Marouane <am0089@uah.edu>
2025-05-19 09:34:50 -0500

    Using SSL

--- local-setup/argocd-github-repos.tf
+++ local-setup/argocd-github-repos.tf
@@ -20,7 +20,7 @@ variable "argocd_applications" {
       app_name      = "nginx-app"
       repo_url      = "https://github.com/amarouane-ABDELHAK/eks-apps"
       target_path   = "app/nginx_app"
-      target_branch = "main"
+      target_branch = "use-ssl"
       private       = false
     }
   ]
