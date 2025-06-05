59f10ec15be29cec501fc3d0c68272cb2962a232
Abdelhak Marouane <am0089@uah.edu>
2025-06-05 14:17:00 -0500

    Deploy to staging from main take 2

--- local-setup/main.tf
+++ local-setup/main.tf
@@ -4,6 +4,8 @@ module "kind" {
   http_ingress_port          = var.http_ingress_port
   https_ingress_port         = var.https_ingress_port
   kind_experimental_provider = var.kind_experimental_provider
+  ssl_private_key_path = var.ssl_private_key_path
+  ssl_certificate_path = var.ssl_certificate_path
 }
 
 
