c9fcf7094510679a8bb1ed1d3111dc5c19665e29
amarouane-ABDELHAK <am0089@uah.edu>
2025-07-13 15:07:25 -0500

    Make auto generated jwts

--- local-setup/main.tf
+++ local-setup/main.tf
@@ -9,25 +9,25 @@ module "kind" {
 }
 
 
-module "argocd" {
-  source                      = "./argocd"
-  depends_on                  = [module.kind]
-  argocd_applications         = var.argocd_applications
-  github_app_private_key_path = var.github_app_private_key_path
-  github_app_id               = var.github_app_id
-  github_app_installation_id  = var.github_app_installation_id
-  domain_name = var.domain_name
-  ssl_private_key_path = var.ssl_private_key_path
-  ssl_certificate_path = var.ssl_certificate_path
-}
+# module "argocd" {
+#   source                      = "./argocd"
+#   depends_on                  = [module.kind]
+#   argocd_applications         = var.argocd_applications
+#   github_app_private_key_path = var.github_app_private_key_path
+#   github_app_id               = var.github_app_id
+#   github_app_installation_id  = var.github_app_installation_id
+#   domain_name = var.domain_name
+#   ssl_private_key_path = var.ssl_private_key_path
+#   ssl_certificate_path = var.ssl_certificate_path
+# }
 
-module "monitoring" {
-  source = "./monitoring"
-  depends_on                  = [module.kind]
-  ssl_private_key_path = var.ssl_private_key_path
-  ssl_certificate_path = var.ssl_certificate_path
-  domain_name = var.domain_name
-  grafana_admin = var.grafana_admin
-  grafana_password = var.grafana_password
-}
+# module "monitoring" {
+#   source = "./monitoring"
+#   depends_on                  = [module.kind]
+#   ssl_private_key_path = var.ssl_private_key_path
+#   ssl_certificate_path = var.ssl_certificate_path
+#   domain_name = var.domain_name
+#   grafana_admin = var.grafana_admin
+#   grafana_password = var.grafana_password
+# }
 
